2c548a7b4cf74b44bd3ba7e48bcc4eba4afc72cf
yashigarg06 <yashi.garg20@gmail.com>
2022-08-25 10:50:46 +0530

    added cart page feature

---
+++ src/main/java/com/qa/openCart/pages/CartPage.java
@@ -0,0 +1,15 @@
+package com.qa.openCart.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.qa.openCart.constants.AppConstants;
+import com.qa.openCart.utils.ElementUtil;
+
+public class CartPage {
+	public void addCart() {
+		System.out.println("add to cart");
+	}
+}
